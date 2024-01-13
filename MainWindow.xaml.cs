c1fdc304a3c508f2755158a9cd4f61f1e4f8257c
Mike Horth <mhorth@me.com>
2024-01-12 21:59:46 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace NetworkMegaConfigurator
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public string SelectedAdapter => _adapters[AdapterList.SelectedIndex].Name;
+
+    List<NetworkInterface> _adapters = new();
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      GetNetworkAdapters();
+    }
+
+    void GetNetworkAdapters()
+    {
+      _adapters = NetworkInterface.GetAllNetworkInterfaces().ToList();
+      foreach (var adapter in _adapters)
+      {
+        AdapterList.Items.Add($"{adapter.Name} - {adapter.Description}");
+      }
+    }
+
+    void btnSetStatic_Click(object sender, RoutedEventArgs e)
+    {
+      string args = $"interface ipv4 set address name=\"{SelectedAdapter}\" static 192.168.1.10";
+      SetIp(args);
+    }
+
+    void btnSetDhcp_Click(object sender, RoutedEventArgs e)
+    {
+      string args = $"interface ipv4 set address name=\"{SelectedAdapter}\" dhcp";
+      SetIp(args);
+    }
+
+    static void SetIp(string args)
+    {
+      Debug.WriteLine(args);
+      Process process = new();
+      process.StartInfo = new ProcessStartInfo("netsh", args);
+      process.StartInfo.UseShellExecute = true;
+      process.StartInfo.Verb = "runas";
+      process.Start();
+    }
+  }
+}
