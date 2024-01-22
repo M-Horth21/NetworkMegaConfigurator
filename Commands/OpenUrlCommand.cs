e968ea504ac5bad80dffa658bcd16d2755712a3b
Mike Horth <mhorth@me.com>
2024-01-21 21:59:25 -0500

    Made clickable footer to open GitHub repo.

---
+++ Commands/OpenUrlCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkMegaConfigurator.Commands
+{
+  class OpenUrlCommand : CommandBase
+  {
+    readonly string _url;
+
+    public OpenUrlCommand(string Url)
+    {
+      _url = Url;
+    }
+
+    public override void Execute(object? parameter)
+    {
+      Process.Start(new ProcessStartInfo()
+      {
+        UseShellExecute = true,
+        FileName = _url
+      });
+    }
+  }
+}
--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,9 +1,11 @@
-﻿using NetworkMegaConfigurator.Stores;
+﻿using NetworkMegaConfigurator.Commands;
+using NetworkMegaConfigurator.Stores;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace NetworkMegaConfigurator.ViewModels
 {
@@ -12,8 +14,12 @@ namespace NetworkMegaConfigurator.ViewModels
     public ViewModelBase CurrentViewModel => _navigationStore.CurrentViewModel;
     readonly NavigationStore _navigationStore;
 
+    public ICommand GoToGitHub { get; }
+
     public MainViewModel(NavigationStore navigationStore)
     {
+      GoToGitHub = new OpenUrlCommand("https://github.com/M-Horth21/NetworkMegaConfigurator");
+
       _navigationStore = navigationStore;
       _navigationStore.CurrentViewModelChanged += HandleCurrentViewModelChanged;
     }
