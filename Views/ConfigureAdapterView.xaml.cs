a05720ade216ab8a9bf3c0be18f4f81f5d972a66
Mike Horth <mhorth@me.com>
2024-01-22 19:28:20 -0500

    Added an inverse bool to visibility converter.

--- Views/ConfigureAdapterView.xaml.cs
+++ Views/ConfigureAdapterView.xaml.cs
@@ -39,4 +39,17 @@ namespace NetworkMegaConfigurator.Views
       return (Visibility)value == Visibility.Visible;
     }
   }
+
+  public class InverseBooleanToVisibilityConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return (Visibility)value == Visibility.Collapsed;
+    }
+  }
 }
