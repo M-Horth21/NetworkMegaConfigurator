1fb4ad197f3853f8d47d4480f6fb7e8556096156
Mike Horth <mhorth@me.com>
2024-01-24 22:22:01 -0500

    Began styling on buttons.

---
+++ Converters/SelectionButtonBooleanToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace NetworkMegaConfigurator.Converters
+{
+    class SelectionButtonBooleanToColorConverter:IValueConverter
+    {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      return (bool)value ? "#FF1E2B3C" : "#FF114B9A";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
